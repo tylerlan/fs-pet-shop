0907d960645fffd4dc7aabccea3da8ee82f53cef
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-30 15:09:38 -0700

    Added scaffolding for create subcommand

--- pets.js
+++ pets.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var petsPath = path.join(__dirname, 'pets.json');
+const fs = require('fs');
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
 
-var node = path.basename(process.argv[0]); // 'node'
-var file = path.basename(process.argv[1]); // 'pets.js'
-var cmd = process.argv[2]; // 'read'
-var index = process.argv[3]; // '0'
+const node = path.basename(process.argv[0]); // 'node'
+const file = path.basename(process.argv[1]); // 'pets.js'
+const cmd = process.argv[2]; // 'read'
 
 /*
 IN TERMINAL:
@@ -23,11 +22,13 @@ process.argv[3] === '0'
 */
 
 if (cmd === 'read') {
+  var index = process.argv[3]; // '0'
+
   fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) throw err;
 
-    var pets = JSON.parse(data);
-    var result = pets[index];
+    let pets = JSON.parse(data);
+    let result = pets[index];
 
     // Throw error if index is out of bounds
     if (result === undefined) {
@@ -35,7 +36,42 @@ if (cmd === 'read') {
     } else {
       return result;
     }
-  });
+  })
+} else if (cmd === 'create') {
+
+  var age = parseInt(process.argv[3]); // 3
+  var kind = process.argv[4]; // 'parakeet'
+  var name = process.argv[5]; // 'Cornflake'
+
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) throw readErr;
+
+    if (age && kind && name) {
+      console.log(`{ age: ${age}, kind: ${kind}, name: ${name}}`);
+    } else {
+      console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
+    }
+
+  })
+
+  /*
+  node pets.js create
+      Usage: node pets.js create AGE KIND NAME
+
+  node pets.js create 3
+      Usage: node pets.js create AGE KIND NAME
+
+  node pets.js create 3 parakeet
+      Usage: node pets.js create AGE KIND NAME
+
+  node pets.js create 3 parakeet Cornflake
+      { age: 3, kind: 'parakeet', name: 'Cornflake' }
+
+  node pets.js read 2
+      { age: 3, kind: 'parakeet', name: 'Cornflake' }
+
+  */
+
 } else {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
   process.exit(1);
