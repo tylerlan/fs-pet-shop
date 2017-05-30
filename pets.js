1bc1a14c7a886309357f4876b60686f5933516f0
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-30 16:59:02 -0700

    Added update subcommand

--- pets.js
+++ pets.js
@@ -27,12 +27,12 @@ if (cmd === 'read') {
   fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) throw err;
 
-    let pets = JSON.parse(data);
-    let result = pets[index];
+    let petsArray = JSON.parse(data);
+    let result = petsArray[index];
 
     // Return the whole array if no index is specified
     if (index === undefined) {
-      console.log(pets);
+      console.log(petsArray);
       // Throw error if index is out of bounds
     } else if (result === undefined) {
       console.error(`Usage: ${node} ${file} read INDEX`);
@@ -44,22 +44,18 @@ if (cmd === 'read') {
 
 } else if (cmd === 'create') {
 
-  var newAge = parseInt(process.argv[3]); // 3
-  var newKind = process.argv[4]; // 'parakeet'
-  var newName = process.argv[5]; // 'Cornflake'
+  let newAge = parseInt(process.argv[3]); // 3
+  let newKind = process.argv[4]; // 'parakeet'
+  let newName = process.argv[5]; // 'Cornflake'
 
   fs.readFile(petsPath, 'utf8', function(readErr, data) {
-
     if (readErr) throw readErr;
-
     if (newAge && newKind && newName) {
       let newPet = { age: newAge, kind: newKind, name: newName};
+      let petsArray = JSON.parse(data);
+      petsArray.push(newPet);
 
-      let pets = JSON.parse(data);
-
-      pets.push(newPet);
-
-      let newPetsData = JSON.stringify(pets);
+      let newPetsData = JSON.stringify(petsArray);
 
       fs.writeFile(petsPath, newPetsData, (writeErr) => {
       if (writeErr) throw writeErr;
@@ -71,6 +67,36 @@ if (cmd === 'read') {
       console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
       process.exit(1);
     }
+  })
+} else if (cmd === 'update') {
+
+  let indexOfPetsData = process.argv[3]; //
+  let newAge = parseInt(process.argv[4]); //
+  let newKind = process.argv[5]; //
+  let newName = process.argv[6]; //
+
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) throw readErr;
+    if (indexOfPetsData && newAge && newKind && newName) {
+      let petsArray = JSON.parse(data);
+      let petToUpdate = petsArray[indexOfPetsData];
+      petToUpdate.age = newAge;
+      petToUpdate.kind = newKind;
+      petToUpdate.name = newName;
+
+      let newPetsData = JSON.stringify(petsArray);
+
+      fs.writeFile(petsPath, newPetsData, (writeErr) => {
+      if (writeErr) throw writeErr;
+      })
+
+      console.log(petToUpdate);
+
+    } else {
+      console.error(`Usage: ${node} ${file} update INDEX AGE KIND NAME`);
+      process.exit(1);
+    }
+
   })
 } else {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
