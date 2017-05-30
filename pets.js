a3a294555d20e549b46aa64222781f8ef39b82e9
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-30 14:12:30 -0700

    Added read subcommand and exit with non-zero exit code

--- pets.js
+++ pets.js
@@ -4,12 +4,37 @@ var fs = require('fs');
 var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 
-fs.readFile(petsPath, 'utf8', function(err, data) {
-  if (err) {
-    throw err;
-  }
+var node = path.basename(process.argv[0]); // 'node'
+var file = path.basename(process.argv[1]); // 'pets.js'
+var cmd = process.argv[2]; // 'read'
+var index = process.argv[3]; // '0'
 
-  var pets = JSON.parse(data);
+/*
+IN TERMINAL:
+-------------------------------
+node pets.js read 0
 
-  console.log(pets);
-});
+IN JS:
+-------------------------------
+process.argv[0] === 'node'
+process.argv[1] === 'pets.js'
+process.argv[2] === 'read'
+process.argv[3] === '0'
+*/
+
+if (cmd === 'read') {
+  console.log(typeof index);
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    var pets = JSON.parse(data);
+
+    console.log(pets[index]);
+  });
+}
+else {
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+}
