24955c177e60c47d9366a667dde8416dfe73c821
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-02 21:22:12 -0700

    Added basic auth

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -4,12 +4,28 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
 
+const auth = require('basic-auth');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 app.disable('x-powered-by');
 app.use(morgan('short'));
 app.use(bodyParser.json());
+
+/* =====================
+        AUTH
+===================== */
+
+app.use((req, res, next) => {
+  var cred = auth(req);
+
+  if (!cred || cred.name !== 'admin' || cred.pass !== 'meowmix') {
+    res.set('WWW-Authenticate', 'Basic realm="Required"')
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+})
 
 /* =====================
     READ / RETRIEVE
