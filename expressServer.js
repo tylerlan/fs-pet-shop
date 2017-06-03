00be7b5b32f87089b266f7729a85f0689a42d5a3
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-02 18:36:08 -0700

    Prettified file a bit

--- expressServer.js
+++ expressServer.js
@@ -2,10 +2,6 @@
 
 const controllers = require('./controllers.js'); // The place where functions are stored.
 
-const fs = require('fs');
-const path = require('path');
-const petsPath = path.join(__dirname, 'pets.json');
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
@@ -17,17 +13,20 @@ app.disable('x-powered-by');
 app.use(morgan('short'));
 app.use(bodyParser.json()); // bodyParser.json() returns a Middleware that enables you to access the body of a request
 
-/* ========= OPERATIONS =============== */
+/* ========= OPERATIONS ============== */
 
 app.get('/pets', controllers.query);
 app.get('/pets/:id', controllers.retrieve);
 app.post('/pets', controllers.create);
 
+/* ===== CATCH-ALL ROUTE HANDLER ===== */
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
 
-/* ========= LISTENING ========== */
+/* ======== LISTENING ================ */
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
